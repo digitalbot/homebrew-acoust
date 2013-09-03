1be859ce6bbbab9337aaadc596d028f04d71cbbb
kosuke <kosuke.n27@gmail.com>
2013-09-03 23:55:05 +0900

    add julius

---
+++ julius.rb
@@ -0,0 +1,44 @@
+require 'formula'
+
+class Julius < Formula
+  homepage 'http://julius.sourceforge.jp/'
+  url 'http://sourceforge.jp/frs/redir.php?f=/julius/59049/julius-4.2.3.tar.gz'
+  sha1 '8b1acae4079cbc5ee32b0536da5d4403361a0676'
+
+  depends_on 'portaudio' => 'universal'
+
+  def patches
+    DATA
+  end
+
+  def install
+    ENV.j1
+    ENV['CFLAGS'] = '-O3 -arch i386'
+    ENV['LDFLAGS'] = '-arch i386'
+    args = %W[
+      --enable-words-int
+      --with-mictype=portaudio
+      --prefix=#{prefix}
+    ]
+
+    system "./configure", *args
+    system "make"
+    system "make install"
+  end
+end
+
+
+__END__
+diff --git a/julius/main.c b/julius/main.c
+index b6208f4..ad919c6 100644
+--- a/julius/main.c
++++ b/julius/main.c
+@@ -194,7 +194,7 @@ main(int argc, char *argv[])
+
+   /* initialize and standby the specified audio input source */
+   /* for microphone or other threaded input, ad-in thread starts here */
+-  if (j_adin_init(recog) == FALSE) return;
++  if (j_adin_init(recog) == FALSE) return 0;
+
+   /* output system information to log */
+   j_recog_info(recog);
